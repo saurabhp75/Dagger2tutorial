456e9b1a939fe440569061a8020f5d15004b6f27
Your Name <you@example.com>
2020-09-27 03:25:27 +0530

    Non working code

--- app/src/main/java/com/ytlabs/dagger2tutorial/MainActivity.java
+++ app/src/main/java/com/ytlabs/dagger2tutorial/MainActivity.java
@@ -6,10 +6,15 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Car car;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // This will give a runtime time exception as "car" is not initialized
+        // We need to inject "car"
+        car.drive();
     }
 }

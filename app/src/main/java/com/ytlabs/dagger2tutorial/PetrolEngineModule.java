327ba1e80a65bc423de6173f1b19b2dd2530bc9b
saurabhp75 <saurabhp75@gmail.com>
2020-09-27 08:53:08 +0530

    Optimize, Injecting an interface using module using binds annotation

--- app/src/main/java/com/ytlabs/dagger2tutorial/PetrolEngineModule.java
+++ app/src/main/java/com/ytlabs/dagger2tutorial/PetrolEngineModule.java
@@ -1,13 +1,16 @@
 package com.ytlabs.dagger2tutorial;
 
+import dagger.Binds;
 import dagger.Module;
-import dagger.Provides;
 
 @Module
-public class PetrolEngineModule {
+public abstract class PetrolEngineModule {
 
-    @Provides
-    Engine provideEngine(PetrolEngine engine) {
-        return engine;
-    }
+    // This is better than Provides as no object of PetrolEngineModule is
+    // created and no method is called, instead PetrolEngine is instantiated directly
+    // It is used when we want to bind an implementation to an interface. Also this
+    // module cannot have provide method as that would require an instance of the module
+    // But we can use static methods as we did previously.
+    @Binds
+    abstract Engine bindEngine(PetrolEngine engine);
 }
